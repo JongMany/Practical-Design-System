a18c7622f065142d35c6c2ea1bf1fd70cc141da5
JongMany <blackberry1114@naver.com>
2025-09-29 15:59:05 +0900

    feat: test 워크스페이스 디자인시스템 테스트

--- apps/test/src/App.tsx
+++ apps/test/src/App.tsx
@@ -1,4 +1,4 @@
-// import { Button } from "@acme/react";
+import { Button } from "@acme/react";
 import "./App.css";
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
     <div className="bg-red-200">
       <h1 className="text-blue-500">Hello World</h1>
       <button className="ds-btn">Click me</button>
-      {/* <Button>Click me React</Button> */}
+      <Button onPress={() => console.log("clicked")}>Click me React</Button>
     </div>
   );
 }
